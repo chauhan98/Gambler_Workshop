76d8df2d0d40ac1c97e7e490580fa6e20b58724e
chauhan98 <cshashank.2598@gmail.com>
2021-10-20 08:47:37 +0530

    Gambler Uc1

---
+++ GamblerUc1.java
@@ -0,0 +1,15 @@
+public class GamblerUc1 {
+	public static void  case1() {
+	int gambler_Initial_Amount = 100;
+	int everyGame_Bet = 1 ;
+		System.out.println("Gambler Initial Amount: " +gambler_Initial_Amount);
+		 System.out.println("Bet on each game: " +everyGame_Bet);
+	}
+
+
+	public static void main(String[] args) {
+	System.out.println("Welcome To Gambling Simulation");
+	case1();
+	}
+}
+
